3c31584b3f50b3ad8b2e0c1ba0075102bdd5a3a7
Nes <147901220+IzNescafe@users.noreply.github.com>
2024-05-14 17:20:55 +0630

    Add files via upload
    updated outside sprite and hitbox

---
+++ GameTest/src/entities/Player.java
@@ -0,0 +1,183 @@
+package entities;
+
+import static utilz.constants.PlayerConstants.*;
+import static utilz.HelpMethods.CanMoveHere;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.util.Scanner;
+
+import main.Game;
+import utilz.LoadSave;
+
+public class Player extends Entity {
+	private BufferedImage[][] animations;
+	private int aniTick, aniIndex, aniSpeed = 25;
+	private int playerAction = IDLE;
+	private boolean moving = false, attacking = false;
+	private boolean left, up, right, down;
+	private float playerSpeed = 2.0f;
+	private int[][] lvlData;
+	private float xDrawOffset = 21 * Game.SCALE;
+	private float yDrawOffset = 4 * Game.SCALE;
+	private static Scanner input = new Scanner(System.in);
+	private static int num;
+	private static String source = null;
+
+	public Player(float x, float y, int width, int height) {
+		super(x, y, width, height);
+		loadAnimations();
+		initHitbox(x, y, 20 * Game.SCALE, 42 * Game.SCALE);
+
+	}
+	
+	public static String ChooseCharacter() {
+		System.out.println("Choose the player: \n1)Uzumaki Naruto\n2)Hatake Kakashi");
+		num = input.nextInt();
+		switch(num) {
+		case 1:
+			source = "sprite1.png";
+			break;
+		case 2:
+			source = "sprite2.png";
+		case 3:
+			source = "Untitled.png";
+			break;
+		}
+		return source;
+	}
+	
+	public void update() {
+		updatePos();
+		updateAnimationTick();
+		setAnimation();
+	}
+
+	public void render(Graphics g) {
+		g.drawImage(animations[playerAction][aniIndex], (int) (hitbox.x - xDrawOffset), (int) (hitbox.y - yDrawOffset), 120, 120, null);
+		drawHitbox(g);
+	}
+
+	private void updateAnimationTick() {
+		aniTick++;
+		if (aniTick >= aniSpeed) {
+			aniTick = 0;
+			aniIndex++;
+			if (aniIndex >= GetSpriteAmount(playerAction)) {
+				aniIndex = 0;
+				attacking = false;
+			}
+
+		}
+
+	}
+
+	private void setAnimation() {
+		int startAni = playerAction;
+
+		if (moving)
+			playerAction = RUNNING;
+		else
+			playerAction = IDLE;
+
+		if (attacking)
+			playerAction = ATTACK_1;
+
+		if (startAni != playerAction)
+			resetAniTick();
+	}
+
+	private void resetAniTick() {
+		aniTick = 0;
+		aniIndex = 0;
+	}
+
+	private void updatePos() {
+		moving = false;
+		if (!left && !right && !up && !down)
+			return;
+
+		float xSpeed = 0, ySpeed = 0;
+
+		if (left && !right)
+			xSpeed = -playerSpeed;
+		else if (right && !left)
+			xSpeed = playerSpeed;
+
+		if (up && !down)
+			ySpeed = -playerSpeed;
+		else if (down && !up)
+			ySpeed = playerSpeed;
+
+//		if (CanMoveHere(x + xSpeed, y + ySpeed, width, height, lvlData)) {
+//			this.x += xSpeed;
+//			this.y += ySpeed;
+//			moving = true;
+//		}
+
+		if (CanMoveHere(hitbox.x + xSpeed, hitbox.y + ySpeed, hitbox.width, hitbox.height, lvlData)) {
+			hitbox.x += xSpeed;
+			hitbox.y += ySpeed;
+			moving = true;
+		}
+
+	}
+
+	private void loadAnimations() {
+
+		BufferedImage img = LoadSave.GetSpriteAtlas(LoadSave.PLAYER_ATLAS);
+
+		animations = new BufferedImage[8][6];
+		for (int j = 0; j < animations.length; j++)
+			for (int i = 0; i < animations[j].length; i++)
+				animations[j][i] = img.getSubimage(i * 100, j * 100, 100, 100);
+
+	}
+
+	public void loadLvlData(int[][] lvlData) {
+		this.lvlData = lvlData;
+	}
+
+	public void resetDirBooleans() {
+		left = false;
+		right = false;
+		up = false;
+		down = false;
+	}
+
+	public void setAttacking(boolean attacking) {
+		this.attacking = attacking;
+	}
+
+	public boolean isLeft() {
+		return left;
+	}
+
+	public void setLeft(boolean left) {
+		this.left = left;
+	}
+
+	public boolean isUp() {
+		return up;
+	}
+
+	public void setUp(boolean up) {
+		this.up = up;
+	}
+
+	public boolean isRight() {
+		return right;
+	}
+
+	public void setRight(boolean right) {
+		this.right = right;
+	}
+
+	public boolean isDown() {
+		return down;
+	}
+
+	public void setDown(boolean down) {
+		this.down = down;
+	}
+
+}
